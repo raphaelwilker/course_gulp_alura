d839e13c16ae6d9669cf25de1f4214cb41659b68
Raphael Araujo/LGESP CNS - SSP(raphael.araujo@lgepartner.com) <raphael.araujo@lge.com>
2019-05-20 10:07:05 -0300

    dev: finsih course

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 const gulp = require('gulp')
   ,imagemin = require('gulp-imagemin')
   ,clean = require('gulp-clean')
-  ,concat = require('gulp-concat')
-  ,htmlReplace = require('gulp-html-replace')
   ,uglify = require('gulp-uglify')
   ,usemin = require('gulp-usemin')
-  ,cssmin = require('gulp-cssmin');
+  ,cssmin = require('gulp-cssmin')
+  ,browserSync = require('browser-sync')
+  ,jshint = require('gulp-jshint')
+  ,jshintStylish = require('jshint-stylish')
+  ,csslint = require('gulp-csslint')
+  ,autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default',['copy'],function(){
-    gulp.start(['build-img','usemin']);
+    gulp.start(['build-img','usemin','server']);
 });
 
 gulp.task('clean', function() {
@@ -31,7 +34,39 @@ gulp.task('usemin', function() {
     return gulp.src('dist/**/*.html')
       .pipe(usemin({
         js: [uglify],
-        css: [cssmin]
+        css: [autoprefixer,cssmin]
       }))
       .pipe(gulp.dest('dist'));
+});
+
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'src'
+        }
+    });
+    gulp.watch('src/**/*').on('change', browserSync.reload);
+
+    gulp.watch('src/js/**/*.js').on('change', function(event) {
+        console.log("Linting " + event.path);
+        gulp.src(event.path)
+            .pipe(jshint())
+            .pipe(jshint.reporter(jshintStylish));
+    });
+
+    gulp.watch('src/css/**/*.css').on('change', function(event) {
+        console.log("Linting " + event.path);
+        gulp.src(event.path)
+            .pipe(csslint())
+            .pipe(csslint.reporter());
+    }); 
+
+    gulp.watch('src/less/**/*.less').on('change', function(event) {
+        var stream = gulp.src(event.path)
+             .pipe(less().on('error', function(erro) {
+               console.log('LESS, erro compilação: ' + erro.filename);
+               console.log(erro.message);
+             }))
+             .pipe(gulp.dest('src/css'));
+    });
 });
